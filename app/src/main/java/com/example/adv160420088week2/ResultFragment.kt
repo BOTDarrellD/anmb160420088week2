bbf4bd84b2e7eba4bebe7f84641a8cc5cdaeefd7
BOTDarrellD <darrell.dominic@gmail.com>
2023-03-14 23:57:24 +0700

    init 3

--- app/src/main/java/com/example/adv160420088week2/ResultFragment.kt
+++ app/src/main/java/com/example/adv160420088week2/ResultFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.Navigation
 import com.google.android.material.textfield.TextInputEditText
 
@@ -19,12 +20,20 @@ class ResultFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val btnReturns= view.findViewById<Button>(R.id.btnReturns)
         btnReturns.setOnClickListener {
             val action = ResultFragmentDirections.actionMainFragment2()
             Navigation.findNavController(it).navigate(action)
+        }
+
+        if(arguments != null) {
+            val playerAnswer =
+                ResultFragmentArgs.fromBundle(requireArguments()).playerAnswer
+            val txtScore = view.findViewById<TextView>(R.id.txtScore)
+            txtScore.text = "Your Score is $playerAnswer"
         }
     }
 }
